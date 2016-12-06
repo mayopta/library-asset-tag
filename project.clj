9ed3e3b6aa243993988983ed1c9c8c5cb1e92634
Gregory Haskins <gregory.haskins@gmail.com>
2016-12-06 13:08:03 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -3,11 +3,14 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
                  [compojure "1.5.1"]
+                 [ring "1.5.0"]
                  [ring/ring-defaults "0.2.1"]
                  [buddy/buddy-auth "1.3.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler library-asset-tag.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  :main ^:skip-aot library-asset-tag.main
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.0"]]}
+             :uberjar {:aot :all}})
---
+++ src/library_asset_tag/main.clj
@@ -0,0 +1,43 @@
+(ns library-asset-tag.main
+  (:require [library-asset-tag.handler :as handler]
+            [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]]
+            [ring.adapter.jetty :refer [run-jetty]])
+  (:gen-class))
+
+(def options
+  [["-h" "--help"]
+   [nil "--port PORT" "The post to host our service endpoint"
+    :default 3000
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 65536) "Must be a number between 0 and 65536"]]])
+
+(defn exit [status msg & rest]
+  (do
+    (apply println msg rest)
+    status))
+
+(defn prep-usage [msg] (->> msg flatten (string/join \newline)))
+
+(defn usage [options-summary]
+  (prep-usage ["Usage: library-asset-tagd [options] action"
+               ""
+               "Options:"
+               options-summary]))
+
+(defn -app [& args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args options)]
+    (cond
+
+      (:help options)
+      (exit 0 (usage summary))
+
+      (not= errors nil)
+      (exit -1 "Error: " (string/join errors))
+
+      :else
+      (let [{:keys [port]} options]
+        (run-jetty handler/app {:port port})))))
+
+(defn -main [& args]
+  (System/exit (apply -app args)))
