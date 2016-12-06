db0bfc0e89c8cbd646c4f800335a00238fa1ce15
Gregory Haskins <gregory.haskins@gmail.com>
2016-12-06 13:28:33 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/library_asset_tag/auth.clj
+++ src/library_asset_tag/auth.clj
@@ -2,18 +2,15 @@
   (:require [buddy.auth.accessrules :refer [error]]))
 
 (defn get-login [session]
-  (println "get-login:" session)
   (if-let [token (:identity session)]
     token
     {:status 401}))
 
 (defn login [session token]
-  (println "login:" session)
   {:status 200
    :session (assoc session :identity token)})
 
 (defn logout [session]
-  (println "logout:" session)
   (if (:identity session)
     {:status 200
      :session (dissoc session :identity)}
