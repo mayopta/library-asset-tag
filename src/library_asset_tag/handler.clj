600b9e2563ac381ee57b4c3009a09ef48511c147
Gregory Haskins <gregory.haskins@gmail.com>
2016-12-06 09:14:41 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/library_asset_tag/handler.clj
+++ src/library_asset_tag/handler.clj
@@ -46,18 +46,6 @@
                     (DELETE "/" {session :session} (logout session)))
            (route/not-found "Not Found")))
 
-;; Define a in-memory relation between tokens and users:
-(def tokens {:2f904e245c1f5 :admin
-             :45c1f5e3f05d0 :foouser})
-
-;; Define an authfn, function with the responsibility
-;; to authenticate the incoming token and return an
-;; identity instance
-
-(defn my-authfn [request token]
-  (let [token (keyword token)]
-    (get tokens token nil)))
-
 ;; Create an instance
 (def backend (backends/session))
 
