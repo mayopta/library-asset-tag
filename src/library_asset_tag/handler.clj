671a408829c07def53796ec8342f76472864b94a
Gregory Haskins <gregory.haskins@gmail.com>
2016-12-06 08:57:06 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/library_asset_tag/handler.clj
+++ src/library_asset_tag/handler.clj
@@ -1,7 +1,10 @@
 (ns library-asset-tag.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.session :refer [wrap-session]]
+            [buddy.auth.backends :as backends]
+            [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]))
 
 (defn- get-inventory-summary []
   (str "Summary:"))
@@ -14,12 +17,51 @@
     (get-inventory-summary)
     (get-inventory-range start end)))
 
+(defn- get-login [session]
+  (println "get-login:" session)
+  (if-let [token (:identity session)]
+    token
+    {:status 401}))
+
+(defn- login [session token]
+  (println "login:" session)
+  {:status 200
+   :session (assoc session :identity token)})
+
+(defn- logout [session]
+  (println "logout:" session)
+  (if (:identity session)
+    {:status 200
+     :session (dissoc session :identity)}
+    {:status 401}))
+
 (defroutes api-routes
   (context "/api/v1" []
            (context "/inventory" []
                     (GET "/" [& params] (get-inventory params))
                     (GET "/:id" [id] (str "Hello " id)))
+           (context "/login" []
+                    (GET "/" {session :session} (get-login session))
+                    (PUT "/" [token :as {session :session}] (login session token))
+                    (DELETE "/" {session :session} (logout session)))
            (route/not-found "Not Found")))
 
+;; Define a in-memory relation between tokens and users:
+(def tokens {:2f904e245c1f5 :admin
+             :45c1f5e3f05d0 :foouser})
+
+;; Define an authfn, function with the responsibility
+;; to authenticate the incoming token and return an
+;; identity instance
+
+(defn my-authfn [request token]
+  (let [token (keyword token)]
+    (get tokens token nil)))
+
+;; Create an instance
+(def backend (backends/session))
+
 (def app
-  (wrap-defaults api-routes site-defaults))
+  (-> (wrap-defaults api-routes api-defaults)
+      wrap-session
+      (wrap-authentication backend)))
