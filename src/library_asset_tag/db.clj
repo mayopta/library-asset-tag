99a959ecd96e9733fe2105bbbf7fe6def322677e
Gregory Haskins <gregory.haskins@gmail.com>
2016-12-07 22:42:26 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/library_asset_tag/db.clj
+++ src/library_asset_tag/db.clj
@@ -10,14 +10,14 @@
 ;;------------------------------------------------------
 (def alloc-assetid
   "Atomically increment the next sequence and simultaneously create an inventory record"
-  #db/fn {:lang :clojure
-          :params [db id context]
-          :code (let [entity (d/entity db :sequence/id)
-                      assetid (or (:sequence/next entity) 1)]
-                  [{:db/id :sequence/id
-                    :sequence/next (inc assetid)}
-                   {:db/id id
-                    :inventory/assetid assetid}])})
+  (d/function {:lang :clojure
+               :params '[db id context]
+               :code '(let [entity (d/entity db :sequence/id)
+                           assetid (or (:sequence/next entity) 1)]
+                       [{:db/id :sequence/id
+                         :sequence/next (inc assetid)}
+                        {:db/id id
+                         :inventory/assetid assetid}])}))
 
 ;;------------------------------------------------------
 ;; install-schema - initializes a new database by installing
