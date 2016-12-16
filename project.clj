930d8a2de2891cae1a9c3b652dbd1337e312215d
Gregory Haskins <gregory.haskins@gmail.com>
2016-12-15 23:01:08 -0500

    Convert to a chestnut style configuration
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject library-asset-tag "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.6.1"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.89" :scope "provided"]
                  [org.clojure/tools.cli "0.3.5"]
                  [compojure "1.5.1"]
                  [ring "1.5.0"]
@@ -12,10 +13,103 @@
                  [clj-http "3.4.1"]
                  [cheshire "5.6.3"]
                  [slingshot "0.12.2"]]
-  :plugins [[lein-ring "0.9.7"]]
+
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.3"]
+            [lein-environ "1.0.3"]]
+
+  :source-paths ["src/clj" "src/cljs" "src/cljc"]
+  :test-paths ["test/clj" "test/cljc"]
+  :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/js"]
+
   :ring {:init library-asset-tag.handler/mock-init
          :handler library-asset-tag.handler/app}
+
   :main ^:skip-aot library-asset-tag.main
+
+  ;; nREPL by default starts in the :main namespace, we want to start in `user`
+  ;; because that's where our development helper functions like (run) and
+  ;; (browser-repl) live.
+  :repl-options {:init-ns user}
+
+  :cljsbuild {:builds
+              [{:id "app"
+                :source-paths ["src/cljs" "src/cljc"]
+
+                :figwheel true
+                ;; Alternatively, you can configure a function to run every time figwheel reloads.
+                ;; :figwheel {:on-jsload "library-asset-tag.core/on-figwheel-reload"}
+
+                :compiler {:main library-asset-tag.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/library-asset-tag.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true}}
+
+               {:id "test"
+                :source-paths ["src/cljs" "test/cljs" "src/cljc" "test/cljc"]
+                :compiler {:output-to "resources/public/js/compiled/testable.js"
+                           :main library-asset-tag.test-runner
+                           :optimizations :none}}
+
+               {:id "min"
+                :source-paths ["src/cljs" "src/cljc"]
+                :jar true
+                :compiler {:main library-asset-tag.core
+                           :output-to "resources/public/js/compiled/library-asset-tag.js"
+                           :output-dir "target"
+                           :source-map-timestamp true
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  ;; When running figwheel from nREPL, figwheel will read this configuration
+  ;; stanza, but it will read it without passing through leiningen's profile
+  ;; merging. So don't put a :figwheel section under the :dev profile, it will
+  ;; not be picked up, instead configure figwheel here on the top level.
+
+  :figwheel {;; :http-server-root "public"       ;; serve static assets from resources/public/
+             ;; :server-port 3449                ;; default
+             ;; :server-ip "127.0.0.1"           ;; default
+             :css-dirs ["resources/public/css"]  ;; watch and update CSS
+
+             ;; Instead of booting a separate server on its own port, we embed
+             ;; the server ring handler inside figwheel's http-kit server, so
+             ;; assets and API endpoints can all be accessed on the same host
+             ;; and port. If you prefer a separate server process then take this
+             ;; out and start the server with `lein run`.
+             :ring-handler user/http-handler
+
+             ;; Start an nREPL server into the running figwheel process. We
+             ;; don't do this, instead we do the opposite, running figwheel from
+             ;; an nREPL process, see
+             ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
+             ;; :nrepl-port 7888
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             :server-logfile "log/figwheel.log"}
+
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-mock "0.3.0"]]}
-             :uberjar {:aot :all}})
+                                  [ring/ring-mock "0.3.0"]
+                                  [figwheel "0.5.4-4"]
+                                  [figwheel-sidecar "0.5.4-4"]
+                                  [com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.12"]]
+                   :plugins [[lein-figwheel "0.5.4-4"]
+                             [lein-doo "0.1.6"]]
+
+                   :source-paths ["dev"]
+                   :repl-options {:nrepl-middleware
+                                  [cemerick.piggieback/wrap-cljs-repl]}}
+             :uberjar {:source-paths ^:replace ["src/clj" "src/cljc"]
+                       :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
+                       :hooks []
+                       :omit-source true
+                       :aot :all}})
