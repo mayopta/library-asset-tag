5b852cb96bdcdbd9f654029d2c90679399505336
Gregory Haskins <gregory.haskins@gmail.com>
2016-12-21 23:06:34 -0500

    Add a current-id display to the home page
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- src/cljs/library_asset_tag/ui/home.cljs
+++ src/cljs/library_asset_tag/ui/home.cljs
@@ -15,33 +15,37 @@
   (render [this]
           (let [{:keys [assets]} (om/props this)]
             (ui/paper nil
-                    (dom/div #js {:className "row center-xs"}
-                             (dom/div #js {:className "col-xs-12"
-                                           :id "action-buttons"}
-                                      (ui/floating-action-button
-                                       {:mini true
-                                        :secondary true
-                                        :on-touch-tap #(client/alloc!)}
-                                       (ic/social-plus-one))))
-                    (dom/div #js {:className "row"}
-                            (dom/div #js {:className "col-xs-12"}
-                                     (ui/table
-                                      {:selectable false
-                                       :multi-selectable false}
-                                      (ui/table-header
-                                       {:display-select-all false
-                                        :adjust-for-checkbox false}
-                                       (ui/table-row
-                                        nil
-                                        (ui/table-header-column nil "ID")
-                                        (ui/table-header-column nil "Date")))
-                                      (ui/table-body
-                                       {:display-row-checkbox false}
-                                       (map (fn [asset]
-                                              (ui/table-row
-                                               nil
-                                               (ui/table-row-column nil asset)
-                                               (ui/table-row-column nil "Today")))
-                                            assets)))))))))
+                      (dom/div #js {:className "row center-xs"}
+                               (dom/div #js {:className "col-xs-2"
+                                             :id "action-buttons"}
+                                        (ui/floating-action-button
+                                         {:mini true
+                                          :secondary true
+                                          :on-touch-tap #(client/alloc!)}
+                                         (ic/social-plus-one))
+                                        )
+                               (dom/div #js {:className "col-xs-8"
+                                            :id "current-id"}
+                                        (dom/h1 nil (first assets))))
+                      (dom/div #js {:className "row"}
+                               (dom/div #js {:className "col-xs-12"}
+                                        (ui/table
+                                         {:selectable false
+                                          :multi-selectable false}
+                                         (ui/table-header
+                                          {:display-select-all false
+                                           :adjust-for-checkbox false}
+                                          (ui/table-row
+                                           nil
+                                           (ui/table-header-column nil "ID")
+                                           (ui/table-header-column nil "Date")))
+                                         (ui/table-body
+                                          {:display-row-checkbox false}
+                                          (map (fn [asset]
+                                                 (ui/table-row
+                                                  nil
+                                                  (ui/table-row-column nil asset)
+                                                  (ui/table-row-column nil "Today")))
+                                               assets)))))))))
 
 (def view (om/factory View))
